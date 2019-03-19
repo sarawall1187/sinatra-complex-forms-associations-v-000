df6db41eaeb87a4c2ab127ff86bc3c602ea410fa
Sara Walker <sarawall1187@gmail.com>
2019-03-19 19:50:14 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -41,7 +41,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner_name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
   binding.pry
     @pet.save
